1694381aa4c8ac9a0bed9d73dd94b6d338e2a257
uzgoto <Honeywaltz.Melodiou@gmail.com>
2020-03-04 23:06:42 +0900

    Retry

--- src/main/java/org/uzgoto/experimental/services/RetryService.java
+++ src/main/java/org/uzgoto/experimental/services/RetryService.java
@@ -5,18 +5,25 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
 
+@Service
 @EnableRetry
 public class RetryService {
 
-  @Retryable(value = IOException.class, maxAttempt = 5, backoff = @Backoff(delay = 1000))
-  public void print() {
+  @Retryable(value = IOException.class, maxAttempts = 5, backoff = @Backoff(delay = 1000))
+  public void print() throws IOException {
+    System.out.println("Try to reading file.");
     var content = Files.newInputStream(Paths.get("C:\\workspace\\foo.bar")).readAllBytes();
     System.out.println(Base64.getEncoder().encodeToString(content));
   }
 
   @Recover
-  void recoverPrint() {
+  public void recoverPrint() {
     System.out.println("retry max attempts is over.");
   }
 }
