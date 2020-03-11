bcc069b1909a0ac14151fdf4734ebb533e013ff8
uzgoto <Honeywaltz.Melodiou@gmail.com>
2020-03-11 23:45:08 +0900

    add shutdown hook

--- src/main/java/org/uzgoto/experimental/DemoApplication.java
+++ src/main/java/org/uzgoto/experimental/DemoApplication.java
@@ -1,13 +1,24 @@
 package org.uzgoto.experimental;
 
+import javax.annotation.PreDestroy;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
-
+  static ConfigurableApplicationContext ctx;
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    ctx = SpringApplication.run(DemoApplication.class, args);
+    System.out.println("Pre register shutdown hook.");
+    ctx.registerShutdownHook();
+    System.out.println("Post register shutdown hook.");
   }
 
+  @PreDestroy
+  static void onExit() {
+    System.out.println("Start preDestroy.");
+    System.out.println("End preDestroy.");
+  }
 }
